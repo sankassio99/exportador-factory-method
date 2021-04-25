cc30155a7cc55344233845c9349bda4502c05fcf
Kássio <55934898+sankassio99@users.noreply.github.com>
2021-04-25 20:22:50 -0300

    Add files via upload

---
+++ src/main/java/Principal.java
@@ -0,0 +1,30 @@
+import com.manoelcampos.exportador.Coluna;
+import com.manoelcampos.exportador.ExportadorListaProduto;
+import com.manoelcampos.exportador.Produto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Executa a aplicação Exportador Simple Factory.
+ * @author Manoel Campos da Silva Filho
+ */
+public class Principal {
+    public static void main(String[] args) {
+        final List<Produto> produtos = new ArrayList<>();
+        produtos.add(new Produto("TV", "LG", "132-A", 120));
+        produtos.add(new Produto("Notebook", "Asus", "New age", 341));
+        produtos.add(new Produto("Smartphone", "Samsung", "Galaxy S10", 214));
+
+        ExportadorListaProduto exportadorPadrao = ExportadorListaProduto.newInstance("html");
+
+        exportadorPadrao.addNewColuna(Principal::colunaModelo, "Modelo");
+
+        System.out.println(exportadorPadrao.exportar(produtos));
+
+    }
+
+    private static String colunaModelo(Produto produto){
+        return produto.getModelo();
+    }
+}
---
+++ src/main/java/com/manoelcampos/exportador/Coluna.java
@@ -0,0 +1,13 @@
+package com.manoelcampos.exportador;
+
+public interface Coluna {
+
+    String getTitulo();
+    void setTitulo(String titulo);
+    String abrir();
+    String fechar();
+    String exportarCabecalho();
+    String exportarDado(Produto produto);
+
+
+}
